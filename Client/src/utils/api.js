8559ad74a70be5237de43ce98909df9d750b4486
GyabaahFelix <felixic360@gmail.com>
2025-10-14 22:58:36 +0000

    Base URL and Auth0 updated

--- Client/src/utils/api.js
+++ Client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "https://aubri-homes-server.vercel.app/api",
+  baseURL: "https://aubri-homes.vercel.app/api",
 });
 
 export const getAllProperties = async () => {
